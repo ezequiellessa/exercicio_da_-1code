f6116dd6f12eb0150ca3c1a2c508d387d55cf34d
Ezequiel Lessa de oliveira <102971778+ezequiellessa@users.noreply.github.com>
2023-01-22 11:14:46 -0300

    exercicios resolvidos
    so falta o da mcdonald

---
+++ 3-Arrays/menorNumero/menornumero.js
@@ -0,0 +1,10 @@
+
+
+function menornumero(){
+
+    let numbers = [5, 7, 2, 9, 3, 8, 1];
+    let smallest = Math.min(...numbers);
+    console.log("Menor número é : " + smallest);
+    
+}
+menornumero()
---
+++ 3-Arrays/ordemAlfabbetica/ordemalfabetica.js
@@ -0,0 +1,6 @@
+function solution(lista) {
+    var listaarr =["Zebra", "Lontra", "Peixe", "Gato"];
+    var ordemAlfabetica = listaarr.sort();
+    console.log(ordemAlfabetica);
+}
+solution()
